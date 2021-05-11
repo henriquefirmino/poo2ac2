600f7b20ea8d956b5f4452c90a37d71874168d12
henriquefirmino <henriquecasarini4@gmail.com>
2021-05-11 14:12:57 -0300

    adicionando eventos

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,8 @@
-INSERT INTO tb_event (name, description, place, start_date, end_date, start_time, end_time, email_contact) VALUES ('Aniversario', 'Aniversario de 5 anos','Rua XV, 1212 – Sorocaba/SP','2021-04-08', '2021-04-08', '16:30:00', '22:00:00','aninha@gmail.com');
-INSERT INTO tb_event (name, description, place, start_date, end_date, start_time, end_time, email_contact) VALUES ('Casamento', 'Casamento Hugo e Mariana','Rua Tereza, 23 – Sorocaba/SP','2021-06-25','2021-06-26','18:00:00', '05:00:00','hugo&mariana@hotmail.com');
-INSERT INTO tb_event (name, description, place, start_date, end_date, start_time, end_time, email_contact) VALUES ('Formatura', 'Formatura','Rua Italia, 19  – Sorocaba/SP','2021-12-12','2021-12-13','18:00:00', '05:00:00','faculdade@hotmail.com');
+INSERT INTO tb_event (name, description, place, start_date, end_date, start_time, end_time, email_contact) VALUES ('Aniversario Debutante', 'Aniversario de 15 anos','Rua XXXX, 111 – Sorocaba/SP','2021-05-15', '2021-05-15', '13:00:00', '23:00:00','bianca15@gmail.com');
+INSERT INTO tb_event (name, description, place, start_date, end_date, start_time, end_time, email_contact) VALUES ('Casamento', 'Casamento Fernando e Sabrina','Rua YYYY, 222 – Sorocaba/SP','2021-07-10','2021-07-11','17:00:00', '06:00:00','sabrina99@gmail.com');
+INSERT INTO tb_event (name, description, place, start_date, end_date, start_time, end_time, email_contact) VALUES ('Formatura', 'Formatura Faculdade','Rua ZZZZ, 333  – Sorocaba/SP','2021-12-18','2021-12-19','18:00:00', '05:00:00','faculdade@gmail.com');
+INSERT INTO tb_event (name, description, place, start_date, end_date, start_time, end_time, email_contact) VALUES ('Halloween', 'Halloween','Rua FFF, 666  – Sorocaba/SP','2021-10-30','2021-10-31','20:00:00', '05:00:00','halloween@gmail.com');
+
 
 
 
