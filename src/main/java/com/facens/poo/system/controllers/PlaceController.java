e4954f19b6ade125bca8c0befb70eb8f23b167de
henriquefirmino <henriquecasarini4@gmail.com>
2021-05-16 22:45:32 -0300

    Place controller e dto

--- src/main/java/com/facens/poo/system/controllers/PlaceController.java
+++ src/main/java/com/facens/poo/system/controllers/PlaceController.java
@@ -1,5 +1,48 @@
 package com.facens.poo.system.controllers;
 
+import java.net.URI;
+
+import com.facens.poo.system.dto.PlaceDTO;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+@RestController
+@RequestMapping("/places")
 public class PlaceController {
+
+    @GetMapping("{id}")
+    public ResponseEntity<PlaceDTO> getPlaceById(@PathVariable Long id) {
+        PlaceDTO dto = service.getPlaceById(id);
+        return ResponseEntity.ok().body(dto);
+    }
+
+    @PostMapping
+    public ResponseEntity<PlaceDTO> insert(@RequestBody PlaceInsertDTO insertDTO) {
+        PlaceDTO dto = service.insertPlace(insertDTO);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.created(uri).body(dto);
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        service.deletePlace(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<PlaceDTO> update(@RequestBody PlaceUpdateDTO updateDTO, @PathVariable Long id) {
+        PlaceDTO dto = service.updatePlace(id, updateDTO);
+        return ResponseEntity.ok().body(dto);
+    }
     
 }
